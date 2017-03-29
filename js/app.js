2014341e75d961c4a2a2e4e293a6c295e212d260
Oniki Yuki <onikiyuki@ip-172-16-1-101.ap-northeast-1.compute.internal>
2017-03-29 14:05:06 +0900

    Lifting State Up終了

--- js/app.js
+++ js/app.js
@@ -37,8 +37,8 @@ class Square extends React.Component {
     }
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
@@ -51,8 +51,13 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
         };
     }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
     render() {
         const status = 'Next player: X';
@@ -87,19 +92,13 @@ class Game extends React.Component {
                     <Board />
                 </div>
                 <div className="game-info">
-                    <div>Instagram</div>
-                    <ol>Instagram</ol>
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
                 </div>
             </div>
         );
     }
 }
-
-
-// ReactDOM.render(
-//     <Game />,
-//     document.getElementById('root')
-// );
 
 ReactDom.render((
     <Game />
