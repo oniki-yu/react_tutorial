00d5c144e45f0bcdbb25547d6a659ac89c1a2409
Oniki Yuki <onikiyuki@ip-172-16-1-101.ap-northeast-1.compute.internal>
2017-03-29 13:27:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-chess
+
+## install
+
+```
+yarn install
+./node_modules/gulp/bin/gulp.js
+```
+`npm`でも動きますが、`yarn`推奨です。
+index.htmlをブラウザでロードすれば、使えます。
---
+++ js/app.js
@@ -0,0 +1,127 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+// class ShoppingList extends React.Component {
+//     render() {
+//         return (
+//          <div className="shopping-list">
+//                 <h1>Shopping List for {this.props.name}</h1>
+//                 <ul>
+//                     <li>Instagram</li>
+//                     <li>WhatsApp</li>
+//                     <li>Oculus</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+//
+// // Validationを定義
+// ShoppingList.propTypes = {
+//     name: React.PropTypes.string.isRequired, // string型で必須
+// };
+// ReactDom.render((
+//     <ShoppingList name="Mark" />
+// ), document.getElementById('root'));
+
+
+
+
+
+class Square extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            value: null,
+        };
+    }
+    render() {
+        return (
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} />;
+    }
+    render() {
+        const status = 'Next player: X';
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>Instagram</div>
+                    <ol>Instagram</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+// ReactDOM.render(
+//     <Game />,
+//     document.getElementById('root')
+// );
+
+ReactDom.render((
+    <Game />
+), document.getElementById('root'));
+
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
